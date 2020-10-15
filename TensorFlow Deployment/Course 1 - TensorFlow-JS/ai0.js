d4184323865346d0f72f8a0d8eeb849b86d83153
Jonas Almeida <jalmeida@mathbiol.org>
2020-10-15 10:36:59 -0400

    week 1 over

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/ai0.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/ai0.js
@@ -2,6 +2,41 @@ console.log('ai0.js loaded at '+ Date())
 
 ai = {}
 
-ai.loadIris=(url="https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")=>{
-    debugger
+ai.loadIrisD3=async(url="iris.csv")=>{
+    let dd=[]
+    await d3.csv('iris.csv',d=>{
+        Object.keys(d).forEach(k=>{
+            if(k.slice(0,5)=='petal'||k.slice(0,5)=='sepal'){
+                d[k]=parseFloat(d[k])
+            }
+        })
+        dd.push(d)
+    })
+    return dd
 }
+
+ai.runIris=async(url="iris.csv")=>{
+    const trainingData = tf.data.csv(url,{
+        columnConfigs:{
+            species:{
+                isLabel:true
+            }
+        }
+    })
+    //return trainingData
+    const convertedData = trainingData.map(({xs,ys})=>{
+        const labels = [
+            ys.species == "setosa" ? 1 :0,
+            ys.species == "virginica" ? 1 :0,
+            ys.species == "versicolor" ? 1 :0
+        ];
+        //console.log(labels)
+        return  {
+            xs:Object.values(xs),
+            ys:Object.values(ys)
+        }
+    }).batch(10)
+    return convertedData
+}
+
+//lala = ai.runIris()
